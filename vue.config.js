abfa6b4584e422944452c3aa815c0f5a22d6c12d
ingdev-fr <damien.will@ingdev.fr>
2023-01-10 16:07:02 +0100

    build initial netlify

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 
 module.exports = defineConfig({
-  // Paramétrage du publicPath 
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '' // je veux enlever la barre pour que les images et assets puissent s'afficher correctement.
-    : '/' , // sinon je renvoie vers la racine du projet
-
   // Paramétrage Sass
   transpileDependencies: true,
   css: {
